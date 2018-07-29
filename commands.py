f9dd13b1cacb76e6b0a4502b9ff46e35ca70740f
Unknown <david@tpalmer.com>
2018-07-28 21:17:09 -0700

    Implemented !translate, fucked up opus_load.py

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 import discord
 import youtube_dl
 import googletrans
+translator = googletrans.Translator()
 
 commands_list = ["hello", "play", "disconnect", "translate"]
 
@@ -48,29 +49,22 @@ async def disconnect(client, message):
 			await vc.disconnect()
 
 async def translate(client, message, params):
-	params_list = params.split(" ")
-	to_be_translated = params_list[0]
-	for x in range(len(params_list)):
-		if x != 0:
-			if params_list[x][:3] == "src":
-				src_lang = params_list[x][4:]
-			if params_list[x][:4] == "dest":
-				dest_lang = params_list[x][5:]
-	if src_lang not in googletrans.LANGUAGES:
-		await self.client.send_message(self.message.channel, "That is not a valid source language.")
-		return
-	if dest_lang not in googletrans.LANGUAGES:
-		await self.client.send_message(self.message.channel, "That is not a valid destination language.")
-		return
-	translated_text = googletrans.translator.translate(to_be_translated, src=src_lang, dest=dest_lang)
+	src_lang = params.pop(-2) #the source language must be the second to last item
+	dest_lang = params.pop(-1) #the destination language must be the last item
+	to_be_translated = " ".join(params) #the text to be translated is what remains
 
-	final_msg = "Original text: \"%s\" (source language: %s)\nTranslated text: \"%s\" (destination language: %s)" % to_be_translated, src_lang, translated_text.text, dest_lang
-	await self.client.send_message(self.message.channel, final_msg)
+	if src_lang not in googletrans.LANGUAGES: #ensures that src_lang is a valid source language
+		await client.send_message(message.channel, "\""+src_lang+"\" is not a valid source language.")
+		return
+	if dest_lang not in googletrans.LANGUAGES: #ensures that dest_lang is a valid destination language
+		await client.send_message(message.channel, "\""+dest_lang+"\" is not a valid destination language.")
+		return
+
+	translated_text = translator.translate(to_be_translated, src=src_lang, dest=dest_lang)
+
+	final_msg = "`Original text: \"%s\" (source language: %s)\nTranslated text: \"%s\" (destination language: %s)`" % (to_be_translated, translated_text.src, translated_text.text, translated_text.dest)
+	await client.send_message(message.channel, final_msg)
 	return
-
-#		msg = "Hello {0.author.mention}".format(message)
-#		await self.client.send_message(self.message.channel, msg)
-
 
 
 def after_song(): # debugging purposes
